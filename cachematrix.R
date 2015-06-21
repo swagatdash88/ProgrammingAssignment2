fe620f97ec4afb45ea9c55c1e6e717606de63842
swagatdash88 <Mac@Macintosh.local>
2015-06-21 12:33:32 +0530

    Final Version

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Following two functions are used to calulate and store 
+## the inverse of a matrix. When there is no change in the 
+## input matrix, inverse is fetched from cache which will save
+## computation time
 
-## Write a short comment describing this function
+## This Function takes a matrix as an input and 
+## generates a list of functions to get and set the 
+## values of matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  
+  x_inverse<<-NULL
   set_mat <- function(y) {
     x <<- y
     x_inverse <<- NULL
@@ -12,6 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
   get_mat <- function() x
   set_inv <- function(x_inv) x_inverse <<- x_inv
   get_inv <- function() x_inverse
+  
+  ## List of get-set functions
   list(set_mat = set_mat, get_mat = get_mat,
        set_inv = set_inv,
        get_inv = get_inv)
@@ -19,18 +25,26 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This Function takes a matrix and function 
+## list (from makeCacheMatrix function) as input. 
+## If the input matrix and that stored in cache are same 
+## and the inverse is available in the cache then inverse
+## fetched and returned from cache. Otherwise, inverse is
+## calculated and stored in cache
 
-cacheSolve <- function(x, ...) {
-   ## Return a matrix that is the inverse of 'x'
-  new_mat <- x$get_mat()
-  new_mat_inv <- x$get_inv()
+cacheSolve <- function(x, func_list) {
+  new_mat <- func_list$get_mat()
+  new_mat_inv <- func_list$get_inv()
+  
+  ## Checks if matrices are identical and inverse 
+  ## is available in cache
   if(identical(new_mat, x) && !is.null(new_mat_inv)) {
     message("getting cached data")
     return(new_mat_inv)
   }
-  data <- x$get_mat()
+  
+  data <- func_list$get_mat()
   new_mat_inv <- solve(new_mat)
-  x$set_inv(new_mat_inv)
+  func_list$set_inv(new_mat_inv)
   new_mat_inv
 }
